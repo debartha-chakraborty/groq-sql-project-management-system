7d9440080ebcf84286821e10ef6940c3e79717cd
souvik-clearspot <souvik@clearspot.ai>
2024-05-12 22:11:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -176,17 +176,6 @@ def delete_task(task_id):
     return jsonify({"message": "Task deleted successfully"})
 
 
-
-
-
-
-
-
-
-
-
-
-
 ########################## JOB ROUTES ##########################
 
 
